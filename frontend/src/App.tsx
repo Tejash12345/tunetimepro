de87f6f9cd1eb3017dc3968bbf16e16c6ac8a181
rudra-789 <21kh1a0403.tejesh@gmail.com>
2025-02-15 13:36:08 +0530

    Your commit message

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,38 @@
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/home/HomePage"
-import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage"
-import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react"
-import MainLayout from "./layout/MainLayout"
-import ChatPage from "./pages/chat/ChatPage"
-import AlbumPage from "./pages/album/AlbumPage"
-import AdminPage from "./pages/admin/AdminPage"
-import {Toaster} from "react-hot-toast";
-import NotFoundPage from "./pages/404/NotFoundPage"
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
+import AlbumPage from "./pages/album/AlbumPage";
+import AdminPage from "./pages/admin/AdminPage";
+import { Toaster } from "react-hot-toast";
+import NotFoundPage from "./pages/404/NotFoundPage";
 
 function App() {
-
   return (
-    <>
+    <div className="min-h-screen flex flex-col bg-background text-foreground px-4 sm:px-6 lg:px-8 scroll-smooth">
       <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}
-        <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />} />
+        <Route
+          path="/sso-callback"
+          element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
+        />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
         <Route path="/admin" element={<AdminPage />} />
+
+        {/* Wrap routes inside MainLayout */}
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/chat" element={<ChatPage />} />
-          <Route path="/albums/:albumId" element={<AlbumPage />}/>
-          <Route path="*" element={<NotFoundPage />}/>
+          <Route path="/albums/:albumId" element={<AlbumPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
-      <Toaster />
-    </>
-  )
+
+      {/* Toast Notifications with mobile-friendly adjustments */}
+      <Toaster position="top-center" toastOptions={{ className: "max-w-[90%] sm:max-w-md" }} />
+    </div>
+  );
 }
 
-export default App
+export default App;
